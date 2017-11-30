e37dc7cbab126059ea81248982e6ce847410c084
Zeno Rocha <hi@zenorocha.com>
2017-11-30 14:49:36 -0200

    Fixes typo

--- ui/assets/scripts/endpoints.js
+++ ui/assets/scripts/endpoints.js
@@ -6,7 +6,7 @@ const address = {
 // Paste Auth Endpoint below //
 	"auth": "",
 // Paste Auth Endpoint above //
-// Paste Data Endpoint above //
+// Paste Data Endpoint below //
 	"data": ""
 // Paste Data Endpoint above //
 }
